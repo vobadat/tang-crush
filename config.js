3c52756ff0c27c373cbda788036bdb6cb2b3eb89
vobadat <88869915+vobadat@users.noreply.github.com>
2021-08-13 11:26:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,58 @@
 const CONFIG = {
-    introTitle: 'Lan Anh à :>',
-    introDesc: Chào mừng em đến với thế giới bóc phốt 
-nhưng người hay ngủ quên
-ngủ nhiều
+
+    introTitle: 'Lan Anh à!',
+
+    introDesc: Chào mừng em đến với thế giới
+
+    bốc phốt những người ngủ nhiều
+
+    Ngủ quên
+p
+    Và cả ngue gục :>
+
+    btnIntro: 'hihi',
+
+    title: Em có ngủ nhiều không, hả?,
+
+    desc: 'Trả lời đi :>
+
+
+    btnYes: 'Có, ngủ nhiều quá trời nhiều,
+
+    btnNo: 'Không cóooo màaaa
+
+    question:'Khi nào hết dịch đi chơi với anh nha :3
+
+    btnReply: 'Trả lời nè
+
+    reply: 'Oke, chắc chắn luôn,
+
+    mess: 'Anh biết em sẽ đi mà :>,
+
+    messDesc: 'Hôm nào hết dịch nhớ đi, khong đi nghỉ chơi,
+
+    btnAccept: 'Okiiiii lun 
+
+    messLink: https://www.facebook.com/vobadatmtpm
+
+}
+
+
+
+
+
+
 
    
-    btnIntro: 'hihi',
-    title: Chọm đáp án đúng',
-    desc: 'Em ngủ có nhiều khong? ',
-    btnYes: 'Nhiểu quá trời nhiều',
-    btnNo: 'Không nhaaa',
-    question: Khi nào hết dịch đi chơi với anh nha :3
-    btnReply : Trả  lời  nè! 
-    reply: Okeee, không đi bị kí đầu luôn
-    mess: Anh biết kiểu gì cũng đi mà
-    messDesc: Nhớ nha, hứa rồi đó
-    btnAccept: 'Okiiiii lun :3
-    messLink: https://www.facebook.com/vobadatmtpm
+
+
+
+
+
+
+
+
+
+
+
+
